cb79f5158fa61311987a16a92e84a5856179b6db
yairgoldy <yair.goldy@gmail.com>
2022-07-04 11:08:12 +0300

    adding files

---
+++ utils.R
@@ -0,0 +1,150 @@
+
+
+
+calcualte_measures <- function(dat,analysis,calculate_CI = T){
+  
+  cohorts <- dat$Cohort %>% levels()
+  df <- broom::tidy(analysis) %>%
+    filter(str_starts(string = term,"Cohort")) %>% 
+    select(Cohort = term, Coef = estimate, std.error) %>%
+    add_row(Cohort = cohorts[1], Coef = 0, std.error = 0) %>% 
+    mutate(Cohort = str_remove(Cohort,"Cohort"),
+           Cohort = factor(Cohort,levels = cohorts)) %>% 
+    arrange(Cohort)
+  
+  absolute_measures <- dat %>% 
+    calc_absolute_measures(analysis,calculate_CI = calculate_CI)
+  
+  relative_measures <- df %>% calc_relative_measures()
+  
+  df <- df %>% 
+    left_join(absolute_measures, by = "Cohort") %>% 
+    left_join(relative_measures, by = c("Cohort","Coef", "std.error"))
+  
+  df <- df %>% 
+    select(Cohort, `Adjusted Rate` = AR, `Marginal VE`, 
+           `Relative Risk`,`Relative Protection`,`Absolute Difference`) %>% 
+    mutate(across( `Marginal VE`:`Absolute Difference`,  function(x){x[1] <- "Reference"; return(x)}))
+  
+  return(df)
+}
+
+calc_relative_measures <- function(df){
+  
+  #Marginal VE
+  df <- df %>% 
+    mutate(
+      est =  round( (1-exp(-Coef))*100,1),
+      lower = round( (1-exp(-Coef+1.96*std.error))*100,0),
+      upper = round( (1-exp(-Coef-1.96*std.error))*100,0),
+      `Marginal VE` = paste0(est,"% \\\n[",lower,"%, ",upper,"%]")
+    ) 
+  
+  #Relative Risk
+  df <- df %>% 
+    mutate(
+      est =  round( exp(-Coef)*1,3),
+      lower = round( exp(-Coef-1.96*std.error)*1,2),
+      upper = round( exp(-Coef+1.96*std.error)*1,2),
+      `Relative Risk` = paste0(est,"\\\n[",lower,", ",upper,"]")
+    ) %>% 
+    select(- est,-lower,-upper)
+  
+  #Relative Protection
+  df <- df %>% 
+    mutate(
+      est =  round( exp(Coef),1),
+      lower = round( exp(Coef-1.96*std.error),0),
+      upper = round( exp(Coef+1.96*std.error),0),
+      `Relative Protection` = paste0(est,"\\\n[",lower,", ",upper,"]")
+    ) %>% 
+    select(- est,-lower,-upper)
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+calc_absolute_measures <- function(dat,analysis,B =1000,calculate_CI = T){
+  
+  cohorts <- dat$Cohort %>% levels()
+  est <- adjusted_rates(dat, analysis, cohorts, index=0 ) %>% 
+    mutate(
+      Cohort = cohorts,
+      `Absolute Difference` = round(AR - AR[1],1)
+    )
+  
+  if(calculate_CI){
+    boot_dat <- tibble(index = 1:B) %>% 
+      mutate(res = map(index, function(ind){adjusted_rates(dat,analysis, cohorts, index=ind )})) %>% 
+      unnest(cols = c(res))  %>% 
+      group_by(index) %>%
+      mutate(
+        `Absolute Difference` = AR - AR[1]
+      ) %>% 
+      ungroup()
+    
+    
+    
+    
+    CI_est <- boot_dat %>%
+      group_by(Cohort) %>% 
+      summarise(
+        lower = round(quantile(AR,probs=c(0.025),na.rm = TRUE),1),
+        upper = round(quantile(AR,probs=c(0.975),na.rm = TRUE),1),
+        lowerD = round(quantile(`Absolute Difference`,probs=c(0.025),na.rm = TRUE),1),
+        upperD = round(quantile(`Absolute Difference`,probs=c(0.975),na.rm = TRUE),1)
+      ) %>% 
+      ungroup()
+    
+    est <- est %>% 
+      left_join(CI_est,by = c("Cohort")) %>% 
+      mutate(
+        AR = paste0( round(AR,1),"\\\n[",lower,", ",upper,"]"),
+        `Absolute Difference` = paste0( round(`Absolute Difference`,1),"\\\n[",lowerD,", ",upperD,"]")
+      ) %>% 
+      select(Cohort,AR,`Absolute Difference`)
+  }
+  return(est)
+  
+}
+adjusted_rates <- function(dat, analysis, cohorts, index= 0){
+  
+  Sigma <- vcov(analysis)
+  Beta <-  coef(analysis)
+  Beta <-  replace_na(Beta,0)
+  Sigma <-  replace_na(Sigma,0)
+  
+  
+  if(index >=1){
+    set.seed(index)
+    analysis$coefficients <-  rmvnorm(n = 1,mean = Beta, sigma = Sigma)
+  }
+  
+  adj_rate <- NULL
+  for(cohort_i in cohorts){
+    ari <- dat %>% 
+      mutate(AR = dat %>%
+               mutate(Cohort = cohort_i) %>%  
+               predict(object = analysis,type = "response"))%>% 
+      summarise(AR = 100000*sum(AR)/sum(N_person)) %>% 
+      mutate(Cohort = cohort_i)
+    adj_rate <- rbind(adj_rate,ari)
+  }
+  
+  if(index == 0) {
+    adj_rate <- adj_rate %>% 
+      mutate(AR = round(AR,2))
+  }
+  
+  
+  return(adj_rate)
+}  
+
